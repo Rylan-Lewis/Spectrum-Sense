62486d15d33524514812f21e0b6caf716eba11e0
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-03-18 00:33:17 +0530

    feat: added signup/login pages

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import NavbarComponent from "./NavbarComponent";
+import LandingMain from "./landing/LandingMain";
 
-export default function Landing() {
+function Landing() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="fixed left-0 right-0 top-0">
-        <NavbarComponent />
-      </div>
-      <div className="w-2/3">
-        <h1 className="text text-justify">
-          <span className="font-semibold">Spectrum Sense</span> is a cutting-edge ML-powered platform <span className="text-slate-400">designed to
-          support early intervention and improve the lives of individuals and
-          their families.</span>
-        </h1>
+    <div>
+      <div className="w-full h-screen bg-gradient-to-b from-black to-slate-500">
+        <LandingMain />
       </div>
     </div>
   );
 }
+
+export default Landing
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormCard from "./register/FormCard";
+
+
+function Register(){
+  return <div>
+    <FormCard />
+  </div>
+}
+
+export default Register
