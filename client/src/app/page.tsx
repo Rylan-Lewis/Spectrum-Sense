8a437ab19d4237b941b13ac14daa978dddd87485
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-04-21 19:07:17 +0530

    fix: landing

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  
+  return (
+    <main className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="w-2/3">
+        <h1 className="text text-justify md:text-3xl md:text-left">
+          <span className="font-semibold">Spectrum Sense</span> <span className="">is a bleeding-edge ML-powered platform</span> <span className="text-slate-400 hover:text-primary-700">designed to
+          support early intervention and improve the lives of individuals and
+          their families.</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
